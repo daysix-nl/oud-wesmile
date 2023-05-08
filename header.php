d81c481c5fe582ccf2ce425aa2d598c4c0c6b521
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-05-08 08:54:24 +0200

    wip

--- header.php
+++ header.php
@@ -9,6 +9,15 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VWP5L10VDW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VWP5L10VDW');
+</script>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
